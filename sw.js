7d27defb75c39fa9872e694148e79174bd1082e3
peterjxie <peter.j.xie@gmail.com>
2026-09-25 08:57:35 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // INTERCITY service worker: cache on first load, serve from cache, refresh in the background when online.
-const CACHE='intercity-0.898';const SHELL=['./','./index.html','./manifest.webmanifest','./intercity-icon-833.png'];
+const CACHE='intercity-0.900';const SHELL=['./','./index.html','./manifest.webmanifest','./intercity-icon-833.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>Promise.all(SHELL.map(u=>c.add(u).catch(()=>{})))).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET'||new URL(r.url).origin!==location.origin)return;
